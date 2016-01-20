f0c265c657cd97112f57ed821897bccb76f9cbea
kasiakw <kasia.kw04@gmail.com>
2016-01-20 11:25:43 +0100

    funkcje zawieranie

--- funkcje-non-spatial/zawieranie.sql
+++ funkcje-non-spatial/zawieranie.sql
@@ -48,9 +48,10 @@ Obiektem zawieranym mo¿e byæ odcinek (type = 0) lub inny wielokat (otype = 1).
 Dla odcinków o_id to gid z tabeli RIVER, zaœ dla wielokatow o_id to geom_nr z tabeli COUNTRY.
 Zaklada sie, ze wielokaty sa wypukle, dlatego procedura polega na
 sprawdzeniu zawierania wierzcholkow wewnatrz wielokata (funkcja zawiera_punkt)
-*/ 
-CREATE OR REPLACE FUNCTION zawiera_obiekt
-   (g_nr IN NUMBER, o_id IN NUMBER, o_type IN NUMBER) 
+*/
+SET SERVEROUTPUT ON
+CREATE OR REPLACE FUNCTION zawiera_wielokat
+   (g_nr IN NUMBER, o_id IN NUMBER) 
     RETURN NUMBER 
     IS 
     res NUMBER(1);
@@ -69,18 +70,8 @@ BEGIN
       select x BULK COLLECT into x1_coords from country where geom_nr = g_nr;
       select y BULK COLLECT into y1_coords from country where geom_nr = g_nr;
 
-      IF o_type = 1 THEN
-        select x BULK COLLECT into x2_coords from country where geom_nr = o_id;
-        select y BULK COLLECT into y2_coords from country where geom_nr = o_id;
-      ELSIF o_type = 0 THEN
-        select x1 into x2_coords(1) from river where gid = o_id;
-        select y1 into y2_coords(1) from river where gid = o_id;
-        select x2 into x2_coords(2) from river where gid = o_id;
-        select y2 into y2_coords(2) from river where gid = o_id;
-      ELSE
-        DBMS_OUTPUT.PUT_LINE('ZLY TYP OBIEKTU ZAWIERANEGO (moze byc 0 lub 1)');
-        RETURN 0;
-      END IF;      
+      select x BULK COLLECT into x2_coords from country where geom_nr = o_id;
+      select y BULK COLLECT into y2_coords from country where geom_nr = o_id;    
       n_out :=  x1_coords.COUNT;
       n_in :=  x2_coords.COUNT;
       
@@ -113,6 +104,82 @@ BEGIN
       RETURN res;     
 END;
 
+SET SERVEROUTPUT ON
+CREATE OR REPLACE FUNCTION zawiera_odcinek
+   (g_nr IN NUMBER, o_id IN NUMBER) 
+    RETURN NUMBER 
+    IS 
+    res NUMBER(1);
+    TYPE coord_array IS VARRAY(10) OF NUMBER(5,2);
+    x1_coords coord_array;
+    y1_coords coord_array;
+    x_point NUMBER(5,2);
+    y_point NUMBER(5,2);
+    x11 NUMBER(5,2);
+    y11 NUMBER(5,2);
+    x22 NUMBER(5,2);
+    y22 NUMBER(5,2);
+    n_out NUMBER(2);
+    j NUMBER(2);
+    flaga BOOLEAN;
+BEGIN
+      select x BULK COLLECT into x1_coords from country where geom_nr = g_nr;
+      select y BULK COLLECT into y1_coords from country where geom_nr = g_nr;
+
+      select x1 into x11 from river where gid = o_id;
+      select y1 into y11 from river where gid = o_id;
+      select x2 into x22 from river where gid = o_id;
+      select y2 into y22 from river where gid = o_id;
+
+        
+      n_out :=  x1_coords.COUNT;
+      
+      j :=  n_out;
+      flaga := FALSE;
+      x_point := x11;
+      y_point := y11;        
+      res := 1;
+        
+      -- dla kazdego punktu wielokata zawierajacego
+      FOR i IN 1 .. n_out LOOP
+        IF
+          ((y1_coords(i) > y_point) != (y1_coords(j) > y_point) AND
+          (x_point < (x1_coords(j) - x1_coords(i)) * (y_point - y1_coords(i)) / (y1_coords(j) - y1_coords(i)) + x1_coords(i)))
+        THEN
+          flaga := NOT flaga;
+        END IF;
+          j := i;
+      END LOOP;
+      
+      IF FLAGA = FALSE THEN
+        res := 0;
+        RETURN res; 
+      END IF;
+        
+      j :=  n_out;
+      flaga := FALSE;
+      x_point := x22;
+      y_point := y22;        
+      res := 1;
+        
+      -- dla kazdego punktu wielokata zawierajacego
+      FOR i IN 1 .. n_out LOOP
+        IF
+          ((y1_coords(i) > y_point) != (y1_coords(j) > y_point) AND
+          (x_point < (x1_coords(j) - x1_coords(i)) * (y_point - y1_coords(i)) / (y1_coords(j) - y1_coords(i)) + x1_coords(i)))
+        THEN
+          flaga := NOT flaga;
+        END IF;
+          j := i;
+      END LOOP;
+      
+      IF FLAGA = FALSE THEN
+        res := 0;
+      END IF;
+              
+    RETURN res;     
+END;
+
 /* aby wypisac bledy
 alter function zawiera_punkt compile plsql_warnings='ENABLE:ALL' reuse settings;
 alter function zawiera_obiekt compile plsql_warnings='ENABLE:ALL' reuse settings;
@@ -124,4 +191,8 @@ where  ur.name = 'ZAWIERA_PUNKT';
 select *
 from   user_errors ur
 where  ur.name = 'ZAWIERA_OBIEKT';
+
+select *
+from   user_errors ur
+where  ur.name = 'ZAWIERA_ODCINEK';
 */
